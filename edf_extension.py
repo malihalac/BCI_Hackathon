f0c64e54dd4ffddfa9108b34a2b3cba644e06826
malihalac <68399488+malihalac@users.noreply.github.com>
2021-06-30 22:54:26 -0400

    feature_extraction_scripts

--- edf_extension.py
+++ edf_extension.py
@@ -7,9 +7,9 @@ Created on Fri Jun 25 10:00:23 2021
 
 import os
 
-directory = "C:/Users/delor/OneDrive/Desktop/Drexel University/General/Hackathon/BCI2021/BCI_hackathon-main/"
+directory = "/Users/malihalac/desktop/BCI_Hackathon/github/BCI_Hackathon/edf_data/"
 
 for file in os.listdir(directory):
     if ".edf" not in file and ".md" not in file and ".py" not in file:
         print(file)
-        os.rename(file, "C:/Users/delor/OneDrive/Desktop/Drexel University/General/Hackathon/BCI2021/BCI_hackathon-main/" + file.replace(" ", "")+ ".edf")
+        os.rename("/Users/malihalac/desktop/BCI_Hackathon/github/BCI_Hackathon/edf_data/" + file, "/Users/malihalac/desktop/BCI_Hackathon/github/BCI_Hackathon/edf_data/" + file.replace(" ", "")+ ".edf")
